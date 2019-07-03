7fdd619662bd9c4842b4f68da39afa997cc49964
Srijan Sharma <srijans38@gmail.com>
2019-07-03 17:16:56 +0530

    Add Bus Model

--- bdata/admin.py
+++ bdata/admin.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
-from bdata.models import Route
+from bdata.models import Route, Bus
 
 class RouteAdmin(admin.ModelAdmin):
     pass
 
+class BusAdmin(admin.ModelAdmin):
+    pass
+
 admin.site.register(Route, RouteAdmin)
+admin.site.register(Bus, BusAdmin)
