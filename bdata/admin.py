9f3ab6cba14e51ab063a946d0b5a53314d616ed1
Srijan Sharma <srijans38@gmail.com>
2019-07-03 16:13:46 +0530

    Register Route Model in admin.py

--- bdata/admin.py
+++ bdata/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from bdata.models import Route
 
-# Register your models here.
+class RouteAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Route, RouteAdmin)
