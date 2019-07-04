af5ca2cf0edbb130c87c3e3fdf0dfa19978cfe31
Srijan Sharma <srijans38@gmail.com>
2019-07-04 10:56:58 +0530

    Add BusLoc Model

--- bdata/models.py
+++ bdata/models.py
@@ -16,3 +16,11 @@ class Bus(models.Model):
 
     def __str__(self):
         return f"{self.bno[:2]} {self.bno[2:4]} {self.bno[4:6]} {self.bno[6:]}"
+
+class BusLoc(models.Model):
+    bus = models.ForeignKey(Bus, on_delete=models.CASCADE)
+    lat = models.FloatField()
+    long = models.FloatField()
+
+    def __str__(self):
+        return f"Bus No : {self.bus.bno}, Latitude : {self.lat} ,  Longitude : {self.long}"
