4473982c7870a4040f5fc303c79a7b195e1dab1d
Srijan Sharma <srijans38@gmail.com>
2019-08-01 01:09:06 +0530

    Changes views.py

--- api/views.py
+++ api/views.py
@@ -3,7 +3,7 @@ from rest_framework import viewsets
 from bdata.models import BusLoc, Bus
 from api.serializers import BusLocSerializer
 
+
 class BusLocViewSet(viewsets.ModelViewSet):
     queryset = BusLoc.objects.all()
     serializer_class = BusLocSerializer
-    
