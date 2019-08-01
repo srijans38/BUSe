ff8d931e3669e53f2a09be8847fa657df19e57b0
Srijan Sharma <srijans38@gmail.com>
2019-08-02 02:27:13 +0530

    Added form action in find

--- bdata/views.py
+++ bdata/views.py
@@ -4,6 +4,8 @@ from bdata.models import Route, Bus
 # Create your views here.
 
 def find(request):
+    if request.method=="POST":
+        return HttpResponse(request.POST.get("route"))
     context = {
         "routes" : Route.objects.all(),
         "buses" : Bus.objects.all()
--- buse/settings.py
+++ buse/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'empoq1v7_iowh_f+_54(e3@xg-m%t8+uth*bc-251sopf@f0!m'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0', '192.168.1.10']
 
 
 # Application definition
