fef4fe3e72128cbff5c64b4fcd41b2865895b716
Srijan Sharma <srijans38@gmail.com>
2019-10-31 15:50:43 +0530

    Added all API routes

--- api/views.py
+++ api/views.py
@@ -1,9 +1,22 @@
 from django.shortcuts import render
 from rest_framework import viewsets
-from bdata.models import BusLoc, Bus
-from api.serializers import BusLocSerializer
+from bdata.models import BusLoc, Bus, Route, BPoint
+from api.serializers import BusLocSerializer, BPointSerializer, BusSerializer, RouteSerializer
 
 
 class BusLocViewSet(viewsets.ModelViewSet):
     queryset = BusLoc.objects.all()
     serializer_class = BusLocSerializer
+
+class BusViewSet(viewsets.ModelViewSet):
+    queryset = Bus.objects.all()
+    serializer_class = BusSerializer
+
+class RouteViewSet(viewsets.ModelViewSet):
+    queryset = Route.objects.all()
+    serializer_class = RouteSerializer
+
+class BPointViewSet(viewsets.ModelViewSet):
+    queryset = BPoint.objects.all()
+    serializer_class = BPointSerializer
+
